ae6f364afd7dad28f20d0c16cb1285bec351aecd
Andreas Lochmann <and.lochmann@googlemail.com>
2014-09-04 20:02:26 +0000

    Shift version of Enigma to 1.21.
    This changes the version number of Enigma
    to 1.21 in the game as well as in the manuals.
    The commit also includes an update in the
    developers' list of the program, though not
    yet in the manuals.
    
    Todo:
    - Update list of developers (level authors
      and translators) in the program.
    - Update list of developers in all manuals.

--- src/main.hh
+++ src/main.hh
@@ -20,7 +20,7 @@
 #ifndef ENIGMA_MAIN_HH
 #define ENIGMA_MAIN_HH
 
-#define ENIGMACOMPATIBITLITY 1.20
+#define ENIGMACOMPATIBITLITY 1.21
 #define PREFFILENAME "enigmarc.xml"
 #define RATINGSFILENAME "ratings.xml"
 #define TRUSTED_RELEASE 0.92
