60ecec40827c632952f6097f71412ee3836d5b43
Andreas Lochmann <and.lochmann@googlemail.com>
2020-11-30 23:44:02 +0100

    Some tweaks for Windows cross-compilation.

--- etc/mxe-configure.sh
+++ etc/mxe-configure.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
-export CC="i686-pc-mingw32-gcc"
-export CXX="i686-pc-mingw32-g++"
+export CC="i686-w64-mingw32-gcc"
+export CXX="i686-w64-mingw32-g++"
 export CXXFLAGS="-g0"
-export RANLIB=i686-pc-mingw32-ranlib
-export AR=i686-pc-mingw32-ar
-export WINDRES=i686-pc-mingw32-windres
-export STRIP=i686-pc-mingw32-strip
+export RANLIB=i686-w64-mingw32-ranlib
+export AR=i686-w64-mingw32-ar
+export WINDRES=i686-w64-mingw32-windres
+export STRIP=i686-w64-mingw32-strip
 cd ..
 make distclean
-PATH=/usr/i686-pc-mingw32/bin:$PATH \
+PATH=/usr/i686-w64-mingw32/bin:$PATH \
 	./configure \
-	--host=i686-pc-mingw32 \
-	--target=i686-pc-mingw32 \
-	--build=i686-pc-linux \
+	--host=i686-w64-mingw32 \
+	--target=i686-w64-mingw32 \
 	--enable-optimize \
-	AR=i686-pc-mingw32-ar \
+	AR=i686-w64-mingw32-ar \
 	$*
+
+#	--build=i686-pc-linux \
 
