723ca2b1140be8cae1949fe47d472952b3c29c00
Andreas Lochmann <and.lochmann@googlemail.com>
2021-03-01 01:00:31 +0100

    Set MicroHei as default font to enable CJK translations.

--- data/models-64.lua
+++ data/models-64.lua
@@ -1,12 +1,16 @@
 TileSize = 64
 SpriteSize = 80
 ShadowSize = 82
+MainFont = "MicroHei/wqy-microhei.ttc"
+-- Alternative fonts are e.g.:
+--   MainFont = "DejaVuSansCondensed.ttf"
+--   MainFont = "vera_sans.ttf"
 
-DefineFont ("timefont", "DejaVuSansCondensed.ttf", 44, "timefont", 180, 180, 180)
-DefineFont ("smallalternative", "DejaVuSansCondensed.ttf", 14, "menufont")
-DefineFont ("modesfont", "DejaVuSansCondensed.ttf", 16, "menufont", 70, 120, 255)
-DefineFont ("menufont", "DejaVuSansCondensed.ttf", 16, "menufont")
-DefineFont ("menufontsel", "DejaVuSansCondensed.ttf", 16, "menufont", 180, 180, 180)
-DefineFont ("statusbarfont", "DejaVuSansCondensed.ttf", 28, "dreamorp24")
+DefineFont ("timefont", MainFont, 44, "timefont", 180, 180, 180)
+DefineFont ("smallalternative", MainFont, 14, "menufont")
+DefineFont ("modesfont", MainFont, 16, "menufont", 70, 120, 255)
+DefineFont ("menufont", MainFont, 16, "menufont")
+DefineFont ("menufontsel", MainFont, 16, "menufont", 180, 180, 180)
+DefineFont ("statusbarfont", MainFont, 28, "dreamorp24")
 
 dofile(FindDataFile("models-2d.lua"))
